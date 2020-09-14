9a2b848925696f62ab882aeee0d37a55e8c8abab
lobasya <naydyonovdanil@gmail.com>
2020-09-14 09:57:06 +0300

    Add no-cache header

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const createServer = async () => {
             res.setHeader('Access-Control-Allow-Origin', '*')
             res.setHeader('Access-Control-Allow-Headers', '*')
             res.setHeader('Access-Control-Allow-Methods', '*')
+            res.setHeader('Cache-Control', 'no-cache')
             next();
         })
         // await updateCounterJob();
